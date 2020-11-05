5e8fae895681cbe93ea9a3d5f797d135aa3547ae
Paul <pauulbertrand@gmail.com>
2020-11-06 00:07:13 +0100

    avancement du programme pour pouvoir afficher un graphique des données en fonction de la date de leur mesure

--- temp.py
+++ temp.py
@@ -1,38 +1,41 @@
-# -*- coding: utf-8
-
-#min (...)
-#max (...)
-
 import csv
 import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#from matplotlib import pyplot as plt
+from datetime import datetime
 
-df = pd.read_csv("/Users/admin/Documents/GitHub/Projet_INFO/EIVP_KM.csv",sep=';')
-print (df)
-#x = df ['noise' ] 
-#y = df ['temp' ] 
+df = pd.read_csv("C:\\Users\Paul\Documents\EIVP\Projet Python/EIVP_KM.csv",sep=';')
+datedépart= datetime.strptime(input("date de départ:"), "%Y-%m-%d")     #date de départ demandée
+datearrivée= datetime.strptime(input("date d'arrivée:"), "%Y-%m-%d")    #date de fin demandée
+NomDeLaDonnée = input("Nom de la donnée:")
+date = []
+DonnéeEtudiée=df[NomDeLaDonnée]
 
-x = df['noise'] 
-y = df['temp']
-plt.plot(df['noise'],df['temp'])
+for elt in df['sent_at']:                                               #on crée une liste des différentes dates sous le bon type : datetime
+    date.append(datetime.strptime(elt[0:-6],"%Y-%m-%d %H:%M:%S"))
+for elt in date:                                                        #on prend les dates pour quelles soient comprises entre la date de départ et celle de fin ET c'est là qu'est le
+    if elt<datedépart:                                                  #problème
+        DonnéeEtudiée.pop(date.index(elt))
+        date.remove(elt)
+    elif elt>datearrivée:
+        DonnéeEtudiée.pop(date.index(elt))
+        date.remove(elt)
+x = date                                                                #on affiche la courbe
+y = df['noise']
+plt.plot(x,y)
 plt.show ()
 
-plt.plot(x,y)
 
-#EIVP_KM ['noise'].plot(kind'temp', tittle ='Noise Vs Temp')
-
+"""
 def convtime(strtime):
-    """Convert a date "YYYY-MM-DD" as a time since EPOCH."""
+    Convert a date "YYYY-MM-DD" as a time since EPOCH.
     listtime = strtime.strip('-')
     tupletime = (listtime[0], listtime[1], listtime[2])
- 
+
     # datetime.datetime.fromtimestamp(tupletime)
     # Cette fonction fait l'inverse de ce que j'attends...
- 
+
     return time
 
-time deltatime ()
-
+time deltatime ()"""
