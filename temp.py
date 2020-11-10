1ee997b028a3a4067d5a1b50930c1d3d12ab3434
Paul <pauulbertrand@gmail.com>
2020-11-10 12:38:18 +0100

    réunion/maj humidex

--- temp.py
+++ temp.py
@@ -15,31 +15,15 @@ NomDeLaDonnée = input("Nom de la donnée:")
 NumCapteur = float(input("Quel capteur prendre:"))
 
 ###Indice humidex
-"""#calcul indice humidex
-
-import math
-def humidex (T):                                              #je définis ma fonction "humidex" à créer, elle dépend du paramètre T ou température (à voir si c'est nécessaire de l'appeler T plutot que température )
-    H= humidex ('temp')                                       #je la nomme "H" pour la suite
-               for T in range(df['temp']))                    #j'utilise les donnés de la température dans le CSV et je nomme la variable "T"
-               H= T + 5/9*(6,112*10exp(7,5*((T)/(237,7+T)))*('T/100)-10      #formule humidex avec une seule variable T
-    return humidex              #return pour terminer les détails de la fonction
-
-print humidex              #j'affiche les résultats, comment on fait pour ajouter une nouvelle colonne avec ces résultats ?
-
-x = df['humidex']                                                        #on affiche la courbe
-y = df['date']         #avec ce que tu as modifié, comment on fait pour récupérer le temps (différence avec les dates)
-plt.plot(x,y)
-plt.show()"""
 
 def humidex(T):
-    return(T + 5/9*(6,112*10*math.exp(7,5*((T)/(237,7+T)))*(T/100)-10))
+    return(T + 5/9*(6.112*10*math.exp(7.5*((T)/(237.7+T)))*(T/100)-10))
 Humidex=[]
 for elt in df['temp']:
     Humidex.append(humidex(float(elt)))
-print(Humidex)
-
 
 ###Tracé d'une donnée d'un capteur en fonction du temps
+
 capteur=df['id']
 date = []
 DonnéeEtudiée=df[NomDeLaDonnée]
