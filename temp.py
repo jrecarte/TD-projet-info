798860a897673c9b0b57b2010521f1384da37065
Paul <pauulbertrand@gmail.com>
2020-11-12 22:42:01 +0100

    enfin réussi à afficher les courbes sur un intervalle de temps rentré au début, et à tracer seulement pour un seul capteur. Minimum et maximum de fait, se poser des questions sur quelle type de moyenne choisir, je ne pense pas qu'une moyenne pondérée soit intéressante

--- temp.py
+++ temp.py
@@ -9,19 +9,19 @@ df = pd.read_csv("C:\\Users\Paul\Documents\EIVP\TD-projet-info/EIVP_KM.csv",sep=
 
 
 ###Données d'entrées
-datedépart= datetime.strptime(input("date de départ:"), "%Y-%m-%d")     #date de départ demandée
-datearrivée= datetime.strptime(input("date d'arrivée:"), "%Y-%m-%d")    #date de fin demandée
+datedépart= datetime.strptime(input("date de départ:"), "%Y-%m-%d")    #date de départ demandée
+datearrivée= datetime.strptime(input("date d'arrivée':"), "%Y-%m-%d")    #date de fin demandée
 NomDeLaDonnée = input("Nom de la donnée:")
 NumCapteur = float(input("Quel capteur prendre:"))
 
 ###Indice humidex
-
+"""
 def humidex(T):
     return(T + 5/9*(6.112*10*math.exp(7.5*((T)/(237.7+T)))*(T/100)-10))
 Humidex=[]
 for elt in df['temp']:
     Humidex.append(humidex(float(elt)))
-
+"""
 ###Tracé d'une donnée d'un capteur en fonction du temps
 
 capteur=df['id']
@@ -32,22 +32,45 @@ for elt in df['sent_at']:                                               #on cré
     date.append(datetime.strptime(elt[0:-6],"%Y-%m-%d %H:%M:%S"))
 
 
+datef=[]
+DonnéeEtudiéef=[]
 
-for elt in date:                                                        #on prend les dates pour quelles soient comprises entre la date de départ et celle de fin ET c'est là qu'est le
-    if elt<datedépart:                                                  #problème
+
+for i in range(len(date)):
+    if date[i]>=datedépart and date[i]<=datearrivée and capteur[i]==NumCapteur:
+        datef.append(date[i])
+        DonnéeEtudiéef.append(DonnéeEtudiée[i])
+
+"""for elt in date:
+    if elt<datetime.strptime(datedépart, "%Y-%m-%d"):
         DonnéeEtudiée.pop(date.index(elt))
+        capteur.pop(date.index(elt))
         date.remove(elt)
-    elif elt>datearrivée:
+    elif elt>datetime.strptime(datearrivée, "%Y-%m-%d"):
         DonnéeEtudiée.pop(date.index(elt))
-        date.remove(elt)
+        capteur.pop(date.index(elt))
+        date.remove(elt)"""
+max=DonnéeEtudiéef[0]
+min=DonnéeEtudiéef[0]
+for i in range(len(DonnéeEtudiéef)):
+    if DonnéeEtudiéef[i]<min:
+        min=DonnéeEtudiéef[i]
+    if DonnéeEtudiéef[i]>max:
+        max=DonnéeEtudiéef[i]
 
-for i in range(len(capteur)):
-    if capteur[i]!= NumCapteur:
-        del DonnéeEtudiée[i]
-        del date[i]
+
+sum=0                                                                           #la moyenne est terrible faut en utiliser une autre
+for elt in DonnéeEtudiéef:
+    sum+=elt
+moy=sum/len(DonnéeEtudiéef)
 
 
-x = date                                                                #on affiche la courbe
-y = DonnéeEtudiée
-plt.plot(x,y)
+
+Moy=[moy for i in range(len(DonnéeEtudiéef))]
+Max=[max for i in range(len(DonnéeEtudiéef))]
+Min=[min for i in range(len(DonnéeEtudiéef))]                                   #on affiche la courbe
+plt.plot(datef,DonnéeEtudiéef)
+plt.plot(datef,Moy)
+plt.plot(datef,Max)
+plt.plot(datef,Min)
 plt.show()
