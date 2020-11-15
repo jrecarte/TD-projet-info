e20c26e16e97237aa3882fbe2b380bb64a4c3701
Paul <pauulbertrand@gmail.com>
2020-11-15 19:22:56 +0100

    Statistiques et tracés des courbes

--- temp.py
+++ temp.py
@@ -8,25 +8,35 @@ from datetime import datetime
 df = pd.read_csv("C:\\Users\Paul\Documents\EIVP\TD-projet-info/EIVP_KM.csv",sep=';')
 
 
+
+###Indice humidex
+
+def humidex(T,H):
+    return(T + 5/9*(6.112*10**(7.5*((T)/(237.7+T)))*(H/100)-10))
+Humidex=[]
+for i in range(len(df['temp'])):
+    Humidex.append(humidex(df['temp'][i],df['humidity'][i]))
+
+
+
 ###Données d'entrées
+
 datedépart= datetime.strptime(input("date de départ:"), "%Y-%m-%d")    #date de départ demandée
 datearrivée= datetime.strptime(input("date d'arrivée':"), "%Y-%m-%d")    #date de fin demandée
 NomDeLaDonnée = input("Nom de la donnée:")
 NumCapteur = float(input("Quel capteur prendre:"))
 
-###Indice humidex
-"""
-def humidex(T):
-    return(T + 5/9*(6.112*10*math.exp(7.5*((T)/(237.7+T)))*(T/100)-10))
-Humidex=[]
-for elt in df['temp']:
-    Humidex.append(humidex(float(elt)))
-"""
+
+
+
 ###Tracé d'une donnée d'un capteur en fonction du temps
 
 capteur=df['id']
 date = []
-DonnéeEtudiée=df[NomDeLaDonnée]
+if NomDeLaDonnée=='humidex':
+    DonnéeEtudiée=Humidex
+else:
+    DonnéeEtudiée=df[NomDeLaDonnée]
 
 for elt in df['sent_at']:                                               #on crée une liste des différentes dates sous le bon type : datetime
     date.append(datetime.strptime(elt[0:-6],"%Y-%m-%d %H:%M:%S"))
@@ -50,6 +60,9 @@ for i in range(len(date)):
         DonnéeEtudiée.pop(date.index(elt))
         capteur.pop(date.index(elt))
         date.remove(elt)"""
+
+
+###Min-Max
 max=DonnéeEtudiéef[0]
 min=DonnéeEtudiéef[0]
 for i in range(len(DonnéeEtudiéef)):
@@ -59,18 +72,61 @@ for i in range(len(DonnéeEtudiéef)):
         max=DonnéeEtudiéef[i]
 
 
-sum=0                                                                           #la moyenne est terrible faut en utiliser une autre
+###Moyenne
+if NomDeLaDonnée=="humidex":
+    mult=1
+    for elt in DonnéeEtudiéef:
+        mult*=elt/100
+    moy=mult**(1/len(DonnéeEtudiéef))*100
+else:                #Pour l'humidité relative utilisé la moyenne géométrique, pour le reste, moyenne arithmétique
+    sum=0
+    for elt in DonnéeEtudiéef:
+        sum+=elt
+    moy=sum/len(DonnéeEtudiéef)
+
+
+###Variance
+somme=0
 for elt in DonnéeEtudiéef:
-    sum+=elt
-moy=sum/len(DonnéeEtudiéef)
+    somme+=(elt-moy)**2                      #on ne peut que si la donnée n'est pas l'humidex n'est ce pas ?
+variance=somme/len(DonnéeEtudiéef)
+ecarttype=variance**0.5
 
+###Médiane
+DonnéeTriée=sorted(DonnéeEtudiéef)
+index=(len(DonnéeTriée)-1)//2
+if len(DonnéeTriée)%2==1:
+    médiane=DonnéeTriée[index]
+else:
+    médiane=(DonnéeTriée[index]+DonnéeTriée[index+1])/2
 
+###Nom pour graphique
+if NomDeLaDonnée=="temp":
+    NomDeLaDonnée="Température"
+elif NomDeLaDonnée=="noise":
+    NomDeLaDonnée="bruit"
+elif NomDeLaDonnée=="humidity":
+    NomDeLaDonnée="Humidité"
+elif NomDeLaDonnée=="lum":
+    NomDeLaDonnée="Luminosité"
+
+titre="Tracé de "+ NomDeLaDonnée +" en fonction du temps"
 
 Moy=[moy for i in range(len(DonnéeEtudiéef))]
 Max=[max for i in range(len(DonnéeEtudiéef))]
-Min=[min for i in range(len(DonnéeEtudiéef))]                                   #on affiche la courbe
-plt.plot(datef,DonnéeEtudiéef)
-plt.plot(datef,Moy)
-plt.plot(datef,Max)
-plt.plot(datef,Min)
+Min=[min for i in range(len(DonnéeEtudiéef))]
+Var=[variance for i in range(len(DonnéeEtudiéef))]
+ET=[ecarttype for i in range(len(DonnéeEtudiéef))]
+Med=[médiane for i in range(len(DonnéeEtudiéef))]
+plt.plot(datef,DonnéeEtudiéef,"b-",label=NomDeLaDonnée)
+plt.plot(datef,Moy,"c-.",label="Moyenne")
+plt.plot(datef,Max,"r:",label="Maximum")
+plt.plot(datef,Min,"g:",label="Minimum")
+# plt.plot(datef,Var)
+# plt.plot(datef,ET)
+plt.plot(datef,Med,"m--",label="Valeur médiane")
+plt.title(titre)
+plt.xlabel("Temps")
+plt.ylabel(NomDeLaDonnée)
+plt.legend()
 plt.show()
